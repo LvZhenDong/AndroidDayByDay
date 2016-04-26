b36bb1f8be0af1b04e800da0134faa1e3f2392f6
lvzhendong <lvzhendong1993@gmail.com>
2016-04-26 22:47:46 +0800

    GuideActivity
    add activity GuideActivity

--- gen/com/kklv/daybyday/R.java
+++ gen/com/kklv/daybyday/R.java
@@ -28,7 +28,8 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_guide=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -37,6 +38,7 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int welcome=0x7f050003;
     }
     public static final class style {
         /** 
@@ -65,5 +67,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /**  LvZhenDong's base theme 
+         */
+        public static final int BaseTheme=0x7f060002;
     }
 }
---
+++ src/com/kklv/daybyday/GuideActivity.java
@@ -0,0 +1,16 @@
+package com.kklv.daybyday;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuideActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_guide);
+	}
+	
+	
+}
